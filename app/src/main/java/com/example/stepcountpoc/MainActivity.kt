6c49e79543346b9e482af605b3ec85e29b0fa28a
prajwal <prajwaljagadeesh18@gmail.com>
2023-01-24 13:15:35 +0530

    Code clean up cone

--- app/src/main/java/com/example/stepcountpoc/MainActivity.kt
+++ app/src/main/java/com/example/stepcountpoc/MainActivity.kt
@@ -5,54 +5,50 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.hardware.Sensor
+import android.hardware.Sensor.TYPE_STEP_COUNTER
+import android.hardware.Sensor.TYPE_STEP_DETECTOR
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
+import android.view.View
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.example.stepcountpoc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
-    // Added SensorEventListener the MainActivity class
-    // Implement all the members in the class MainActivity
-    // after adding SensorEventListener
 
-    // we have assigned sensorManger to nullable
     private var sensorManager: SensorManager? = null
-
-    // Creating a variable which will give the running status
-    // and initially given the boolean value as false
     private var running = false
-
-    // Creating a variable which will counts total steps
-    // and it has been given the value of 0 float
-    private var totalSteps = 0f
-
-    // Creating a variable  which counts previous total
-    // steps and it has also been given the value of 0 float
-    private var previousTotalSteps = 0f
+    var count = 0
 
     private val PHYISCAL_ACTIVITY = 23
 
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        if(ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACTIVITY_RECOGNITION
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
             //ask for permission
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                requestPermissions(arrayOf(Manifest.permission.ACTIVITY_RECOGNITION), PHYISCAL_ACTIVITY)
+                requestPermissions(
+                    arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                    PHYISCAL_ACTIVITY
+                )
             };
-        }else{
-            loadData()
-            resetSteps()
+        } else {
+            addListenerAndResetValues()
         }
-
 
 
         // Adding a context of SENSOR_SERVICE as Sensor Manager
@@ -60,107 +56,82 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == PHYISCAL_ACTIVITY){
-            loadData()
-            resetSteps()
+        if (requestCode == PHYISCAL_ACTIVITY) {
+            addListenerAndResetValues()
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onResume() {
         super.onResume()
+        initializeSensors()
+        addListenerAndResetValues()
+    }
+
+    private fun initializeSensors() {
         running = true
-
-        // Returns the number of steps taken by the user since the last reboot while activated
-        // This sensor requires permission android.permission.ACTIVITY_RECOGNITION.
-        // So don't forget to add the following permission in AndroidManifest.xml present in manifest folder of the app.
-        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-
-
-        if (stepSensor == null) {
+        val stepCounterSensor = sensorManager?.getDefaultSensor(TYPE_STEP_COUNTER)
+        val stepDetectorSensor = sensorManager?.getDefaultSensor(TYPE_STEP_DETECTOR)
+        if (stepCounterSensor == null || stepDetectorSensor == null) {
             // This will give a toast message to the user if there is no sensor in the device
             Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
         } else {
             // Rate suitable for the user interface
-            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_FASTEST)
-            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_STATUS_ACCURACY_HIGH)
-            resetSteps()
+            sensorManager?.registerListener(
+                this,
+                stepCounterSensor,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+            sensorManager?.registerListener(
+                this,
+                stepCounterSensor,
+                SensorManager.SENSOR_STATUS_ACCURACY_HIGH
+            )
+            sensorManager?.registerListener(
+                this,
+                stepDetectorSensor,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+            sensorManager?.registerListener(
+                this,
+                stepDetectorSensor,
+                SensorManager.SENSOR_STATUS_ACCURACY_HIGH
+            )
         }
-
     }
 
-    var count = 0
+
     override fun onSensorChanged(event: SensorEvent?) {
-
-        // Calling the TextView that we made in activity_main.xml
-        // by the id given to that TextView
-        var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
-
         if (running) {
-            totalSteps = event!!.values[0]
+            if (event?.sensor?.type == TYPE_STEP_COUNTER) {
+                binding.tvTotalStepsValue.text = event.values[0].toInt().toString()
+                binding.progressBar.visibility = View.GONE
+            } else if (event?.sensor?.type == TYPE_STEP_DETECTOR) {
+                count += event.values[0].toInt()
+                binding.tvStepsTaken.text = count.toString()
+                binding.progressBar.visibility = View.VISIBLE
 
-            // Current steps are calculated by taking the difference of total steps
-            // and previous steps
-            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
-
-            // It will show the current steps to the user
-//            tv_stepsTaken.text = ("$currentSteps")
-            count += event.values[0].toInt()
-            tv_stepsTaken.text = ("$count")
+            }
         }
 
     }
 
-    fun resetSteps() {
-        val tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
-        tv_stepsTaken.setOnClickListener {
+    private fun addListenerAndResetValues() {
+
+        binding.tvStepsTaken.setOnClickListener {
             // This will give a toast message if the user want to reset the steps
             Toast.makeText(this, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
         }
 
-        tv_stepsTaken.setOnLongClickListener {
-
-            previousTotalSteps = totalSteps
-
+        binding.tvStepsTaken.setOnLongClickListener {
             // When the user will click long tap on the screen,
             // the steps will be reset to 0
-            tv_stepsTaken.text = 0.toString()
-
-            // This will save the data
-            val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-
-            val editor = sharedPreferences.edit()
-            editor.putFloat("key1", 0f)
-            editor.apply()
+            binding.tvStepsTaken.text = 0.toString()
             count = 0
             true
         }
     }
 
-    private fun saveData() {
-
-        // Shared Preferences will allow us to save
-        // and retrieve data in the form of key,value pair.
-        // In this function we will save data
-        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-
-        val editor = sharedPreferences.edit()
-        editor.putFloat("key1", previousTotalSteps)
-        editor.apply()
-    }
-
-    private fun loadData() {
-
-        // In this function we will retrieve data
-        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val savedNumber = sharedPreferences.getFloat("key1", 0f)
-
-        // Log.d is used for debugging purposes
-        Log.d("MainActivity", "$savedNumber")
-
-//        previousTotalSteps = savedNumber
-        previousTotalSteps = savedNumber
-    }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         // We do not have to write anything in this function for this app
